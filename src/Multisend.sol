58fe06ef37876dfc8d10487ea868cab7c2c0da3f
Sebastian T F <sebastian.tharakan97@gmail.com>
2023-11-06 15:53:46 +0530

    perf: ⛽ use assembly for token transfers

--- src/Multisend.sol
+++ src/Multisend.sol
@@ -16,9 +16,9 @@ contract Multisend {
         address[] calldata recipients,
         uint256[] calldata values
     ) external payable {
-        // transfer ether to recipients
         assembly {
             if recipients.length {
+                /* transfer ether to recipients */
                 // end = start of recipients array + length * 32 bytes
                 let end := add(recipients.offset, shl(5, recipients.length)) // shl by 5 is the same as mul by 32
 
@@ -54,7 +54,7 @@ contract Multisend {
                     }
                 }
 
-                // return excess ether to sender
+                /* return excess ether to sender */
                 if gt(balance(address()), 0) {
                     if iszero(
                         call(gas(), caller(), balance(address()), 0, 0, 0, 0)
@@ -71,10 +71,11 @@ contract Multisend {
         address[] calldata recipients,
         uint256[] calldata values
     ) external {
-        // calculate total amount of tokens to transfer
-        uint256 total = 0;
         assembly {
             if recipients.length {
+                /* calculate total amount of tokens to transfer */
+                let total := 0
+
                 // end = start of recipients array + length * 32 bytes
                 let end := add(recipients.offset, shl(5, recipients.length)) // shl by 5 is the same as mul by 32
 
@@ -97,16 +98,46 @@ contract Multisend {
                         break
                     }
                 }
-            }
-        }
 
-        // transfer tokens to this contract
-        require(token.transferFrom(msg.sender, address(this), total));
-        // transfer tokens to recipients
-        for (uint256 i = 0; i < recipients.length; ) {
-            require(token.transfer(recipients[i], values[i]));
-            unchecked {
-                ++i;
+                /* transfer total tokens to this contract */
+                // prepare calldata
+                mstore(0x00, hex"23b872dd") // transferFrom(address from, address to, uint256 amount)
+                mstore(0x04, caller()) // msg.sender
+                mstore(0x24, address()) // this contract
+                mstore(0x44, total)
+
+                if iszero(call(gas(), token, 0, 0, 0x64, 0, 0)) {
+                    revert(0, 0)
+                }
+
+                /* transfer tokens to recipients */
+                // offsets of first recipient and value
+                recipientOffset := recipients.offset
+                valueOffset := values.offset
+
+                // infinite loop with break at end
+                for {
+
+                } 1 {
+
+                } {
+                    /* transfer total tokens to this contract */
+                    // prepare calldata
+                    mstore(0x00, hex"a9059cbb") // transfer(address to, uint256 value)
+                    mstore(0x04, calldataload(recipientOffset))
+                    mstore(0x24, calldataload(valueOffset))
+
+                    if iszero(call(gas(), token, 0, 0, 0x44, 0, 0)) {
+                        revert(0, 0)
+                    }
+
+                    recipientOffset := add(recipientOffset, 0x20)
+                    valueOffset := add(valueOffset, 0x20)
+
+                    if iszero(lt(recipientOffset, end)) {
+                        break
+                    }
+                }
             }
         }
     }
