920e2798b8dfca8c68dc750bda08866d4fade1ec
Sebastian T F <sebastian.tharakan97@gmail.com>
2023-11-06 14:47:52 +0530

    test: 🧪 add test for Multisend

---
+++ test/Multisend.t.sol
@@ -0,0 +1,56 @@
+pragma solidity 0.8.19;
+
+import {Multisend, IERC20} from "./../src/Multisend.sol";
+import {Token} from "./Token.sol";
+import "forge-std/Test.sol";
+
+contract MultisendTest is Test {
+    Multisend multisend;
+    Token token;
+    address user = vm.addr(0x1337);
+    uint256 totalTokens = 1000;
+    uint256 totalEther = totalTokens * 0.001 ether;
+
+    function setUp() public {
+        token = new Token();
+        token.transfer(user, totalTokens);
+        multisend = new Multisend();
+    }
+
+    function test_multisendToken() public {
+        vm.startPrank(user);
+        token.approve(address(multisend), totalTokens);
+
+        address[] memory recipients = new address[](totalTokens);
+        uint256[] memory amounts = new uint256[](totalTokens);
+        for (uint256 i = 0; i < totalTokens; i++) {
+            recipients[i] = vm.addr(i + 1);
+            amounts[i] = 1;
+        }
+        multisend.multisendToken(IERC20(address(token)), recipients, amounts);
+        vm.stopPrank();
+
+        for (uint256 i = 0; i < totalTokens; i++) {
+            assertEq(token.balanceOf(recipients[i]), 1);
+        }
+    }
+
+    function test_multisendEther() public {
+        payable(user).transfer(totalEther);
+        vm.startPrank(user);
+
+        address[] memory recipients = new address[](totalTokens);
+        uint256[] memory amounts = new uint256[](totalTokens);
+        for (uint256 i = 0; i < totalTokens; i++) {
+            recipients[i] = vm.addr(i + 1);
+            amounts[i] = 0.001 ether;
+        }
+
+        multisend.multisendEther{value: totalEther}(recipients, amounts);
+        vm.stopPrank();
+
+        for (uint256 i = 0; i < totalTokens; i++) {
+            assertEq(address(recipients[i]).balance, 0.001 ether);
+        }
+    }
+}
